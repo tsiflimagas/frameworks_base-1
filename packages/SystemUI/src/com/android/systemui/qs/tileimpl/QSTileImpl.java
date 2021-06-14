PotatoProject/frameworks_base

base: qs: Apply bg to active qs icons
Change-Id: Ib22d5d490fb4856ab4cdf0a033c4373374529214
Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -31,6 +31,8 @@ import android.metrics.LogMaker;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.os.UserHandle;
+import android.provider.Settings.System;
 import android.service.quicksettings.Tile;
 import android.text.format.DateUtils;
 import android.util.ArraySet;
@@ -375,6 +377,18 @@ public abstract class QSTileImpl<TState extends State> implements QSTile {
     public abstract CharSequence getTileLabel();
 
     public static int getColorForState(Context context, int state) {
+        int activeDefault = Utils.getColorAttr(context, android.R.attr.colorPrimary);
+
+        boolean setQsFromWall = System.getIntForUser(context.getContentResolver(),
+                    System.QS_PANEL_BG_USE_WALL, 0, UserHandle.USER_CURRENT) == 1;
+        boolean setQsFromResources = System.getIntForUser(context.getContentResolver(),
+                    System.QS_PANEL_BG_USE_FW, 1, UserHandle.USER_CURRENT) == 1;
+
+        int qsBackGroundColor = System.getIntForUser(context.getContentResolver(),
+                System.QS_PANEL_BG_COLOR, activeDefault, UserHandle.USER_CURRENT);
+        int qsBackGroundColorWall = System.getIntForUser(context.getContentResolver(),
+                System.QS_PANEL_BG_COLOR_WALL, activeDefault, UserHandle.USER_CURRENT);
+
         switch (state) {
             case Tile.STATE_UNAVAILABLE:
                 return Utils.getDisabled(context,
@@ -382,7 +396,14 @@ public abstract class QSTileImpl<TState extends State> implements QSTile {
             case Tile.STATE_INACTIVE:
                 return Utils.getColorAttr(context, android.R.attr.textColorSecondary);
             case Tile.STATE_ACTIVE:
-                return Utils.getColorAttr(context, android.R.attr.colorPrimary);
+                if (setQsFromResources) {
+                    return Utils.getColorAttr(context, android.R.attr.colorPrimary);
+                } else {
+                    if (setQsFromWall)
+                        return qsBackGroundColorWall;
+                    else
+                        return qsBackGroundColor;
+                }
             default:
                 Log.e("QSTile", "Invalid state " + state);
                 return 0;

tsiflimagas/frameworks_base-1

Signed-off-by: Joey Huab <joey@evolution-x.org>

packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -32,6 +32,7 @@ import com.android.systemui.R;
 import android.annotation.CallSuper;
 import android.annotation.NonNull;
 import android.app.ActivityManager;
+import android.app.WallpaperManager;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
@@ -41,6 +42,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.provider.Settings.System;
 import android.service.quicksettings.Tile;
 import android.text.format.DateUtils;
 import android.util.ArraySet;
@@ -61,6 +63,7 @@ import com.android.settingslib.Utils;
 import com.android.systemui.Dependency;
 import com.android.systemui.Dumpable;
 import com.android.systemui.Prefs;
+import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.qs.DetailAdapter;
 import com.android.systemui.plugins.qs.QSIconView;
@@ -506,13 +509,29 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
      */
     public abstract CharSequence getTileLabel();
 
+    private static int getWallpaperColor() {
+        final SysuiColorExtractor colorExtractor = Dependency.get(SysuiColorExtractor.class);
+        // TODO: Find a way to trigger setBackground on lock event, and use FLAG_LOCK there
+        return colorExtractor.getWallpaperColors(WallpaperManager.FLAG_SYSTEM).getPrimaryColor().toArgb();
+    }
+
     public static int getColorForState(Context context, int state) {
+        int activeDefault = Utils.getColorAttrDefaultColor(context, android.R.attr.colorPrimary);
         int setQsUseNewTint = Settings.System.getIntForUser(context.getContentResolver(),
-                Settings.System.QS_PANEL_BG_USE_NEW_TINT, 0, UserHandle.USER_CURRENT);
+                System.QS_PANEL_BG_USE_NEW_TINT, 0, UserHandle.USER_CURRENT);
         int qsTileStyle = Settings.System.getIntForUser(context.getContentResolver(),
-                Settings.System.QS_TILE_STYLE, 0, UserHandle.USER_CURRENT);
+                System.QS_TILE_STYLE, 0, UserHandle.USER_CURRENT);
         boolean shouldDisco = Settings.System.getIntForUser(context.getContentResolver(),
-                Settings.System.QS_TILES_BG_DISCO, 0, UserHandle.USER_CURRENT) == 1;
+                System.QS_TILES_BG_DISCO, 0, UserHandle.USER_CURRENT) == 1;
+        boolean setQsFromWall = System.getIntForUser(context.getContentResolver(),
+                System.SYSUI_COLORS_ACTIVE, 0, UserHandle.USER_CURRENT) == 1;
+        boolean setQsFromResources = System.getIntForUser(context.getContentResolver(),
+                System.QS_PANEL_BG_USE_FW, 1, UserHandle.USER_CURRENT) == 1;
+
+        int qsBackGroundColor = System.getIntForUser(context.getContentResolver(),
+                System.QS_PANEL_BG_COLOR, activeDefault, UserHandle.USER_CURRENT);
+        int qsBackGroundColorWall = getWallpaperColor();
+
         switch (state) {
             case Tile.STATE_UNAVAILABLE:
                 return Utils.getDisabled(context,
@@ -529,7 +548,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                 if (qsTileStyle == 7 || qsTileStyle == 9 || qsTileStyle == 10 || qsTileStyle == 12 ||
                         qsTileStyle == 13 || qsTileStyle == 14) {
                     return Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent);
-                } else {
+                } else if (setQsFromResources) {
                     if (shouldDisco) {
                         return Utils.getColorAttrDefaultColor(context, android.R.attr.colorPrimary);
                     } else if (setQsUseNewTint == 1 && !shouldDisco) {
@@ -538,6 +557,12 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                         return context.getResources().getColor(R.color.qs_tile_oos);
                     } else {
                         return Utils.getColorAttrDefaultColor(context, android.R.attr.colorPrimary);
+                    }
+                } else {
+                    if (setQsFromWall) {
+                        return qsBackGroundColorWall;
+                    } else {
+                        return qsBackGroundColor;
                     }
                 }
             default: